48278ba3fa54bebd5bbc7dbffffea9f96ddf0e71
AriiMe <57349348+AriiMe@users.noreply.github.com>
2021-11-17 10:49:13 +0100

    creating hook for auth

---
+++ Screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const LoginScreen = () => {
+    return (
+        <View>
+            <Text>LogIn to the App</Text>
+        </View>
+    )
+}
+
+export default LoginScreen
---
+++ StackNavigator.js
@@ -0,0 +1,26 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React from 'react'
+import ChatScreen from './Screens/ChatScreen';
+import HomeScreen from './Screens/HomeScreen';
+import LoginScreen from './Screens/LoginScreen';
+
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => {
+    const user = false;
+    return (
+        <Stack.Navigator>
+            {user ? (
+                <>
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                    <Stack.Screen name="Chat" component={ChatScreen} />
+                </>
+            ) : <Stack.Screen name="LogIn" component={LoginScreen} />}
+
+
+        </Stack.Navigator>
+    )
+}
+
+export default StackNavigator
